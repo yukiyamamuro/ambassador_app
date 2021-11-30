2a868f3a7efd1785f78ff6bc6dfb135142322554
山室 友樹 <yuki.sumaho@gmail.com>
2021-11-30 12:23:39 +0900

    add air

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello, World 👋!!")
 	})
 
 	app.Listen(":3000")
