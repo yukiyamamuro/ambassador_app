1471680398d1aa0a7f06a9e370f3b81d30040b02
山室 友樹 <yuki.sumaho@gmail.com>
2021-11-30 12:18:16 +0900

    migration

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"ambassador/src/database"
+
 	"github.com/gofiber/fiber/v2"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	_, err := gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
-
-	if err != nil {
-		panic("could not connect with database")
-	}
+	database.Connect()
+	database.AutoMigrate()
 
 	app := fiber.New()
 
