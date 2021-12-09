4c34bfbc20c0a5ddbd2758def797c8ed978114b4
山室 友樹 <yuki.sumaho@gmail.com>
2021-12-09 12:15:06 +0900

    sub

---
+++ src/commands/populateProducts.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"ambassador/src/database"
+	"ambassador/src/models"
+	"math/rand"
+
+	"github.com/bxcodec/faker/v3"
+)
+
+func mainn() {
+	database.Connect()
+
+	for i := 0; i < 30; i++ {
+		product := models.Product{
+			Title:       faker.Sentence(),
+			Description: faker.Sentence(),
+			Image:       faker.URL(),
+			Price:       float64(rand.Intn(90) + 10),
+		}
+
+		database.DB.Create(&product)
+	}
+}
