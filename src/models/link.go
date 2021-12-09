cc603eb8a6a277fc7e68410cf5fb24328d607997
山室 友樹 <yuki.sumaho@gmail.com>
2021-12-09 12:19:32 +0900

    add link

--- src/models/link.go
+++ src/models/link.go
@@ -1,1 +1,9 @@
 package models
+
+type Link struct {
+	Model
+	Code    string    `json:"code"`
+	UserId  uint      `json:"user_id"`
+	User    User      `json:"user" gorm:"foreignKey:UserId"`
+	Product []Product `josn:"products" gorm"many2many:link_products"`
+}
